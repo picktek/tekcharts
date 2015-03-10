162d07ebd4e25611f5ac08e218a4af4b9579ffc7
MakaLezhava <ma_lezhava@cu.edu.ge>
2015-03-10 16:07:10 +0400

    Maka's first commit

--- lib/config.php
+++ lib/config.php
@@ -6,6 +6,6 @@
  *
  */
 
-    define('TC_SYSTEM_PATH', drupal_get_path('module', 'tekcharts4').'/src');
-    //define('TC_SYSTEM_PATH', 'C:\\Code.Workspace\\TekCharts\\src');
+    //define('TC_SYSTEM_PATH', drupal_get_path('module', 'tekcharts4').'/src');
+    define('TC_SYSTEM_PATH', '/Users/maka/Documents/tekcharts');
 ?>
--- samples/bar.php
+++ samples/bar.php
@@ -1,6 +1,6 @@
 <?php
 
-  require_once (dirname(__FILE__) . '/src/tekCharts.php');
+  require_once (dirname(__FILE__) . '/../lib/tekCharts.php');
 
   function req_def ($varname, $defval) {
     return empty($_GET[$varname]) ? $defval : $_GET[$varname];
